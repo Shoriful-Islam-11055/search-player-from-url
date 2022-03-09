fa0cb898e4be342fed669b9ca54d7b260f2c1875
Shoriful Islam <shoriful15-11055@diu.edu.bd>
2022-03-09 23:33:08 +0600

    search click right sight clear

--- JS/script.js
+++ JS/script.js
@@ -4,17 +4,24 @@ const searchPlayer = () =>{
     // console.log(url);
     inputField.value = '';
 
-    
+    // clear details information when search player
+    const playerInfoDetails = document.getElementById('player-info');
+    playerInfoDetails.textContent = '';
+    document.getElementById('female-player').style.display = "none";
+    document.getElementById('male-player').style.display = "none";
+
+
     const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${Playername}`
     fetch(url)
     .then(res => res.json())
     .then(data => displayplayer(data.player));      
 }
 
+//Show all searching player 
 const displayplayer = players => {
     const playerCardField = document.getElementById('players-card');
     playerCardField.textContent = '';
-
+    
     for(const singlePlayer of players){
         // console.log(singlePlayer.idPlayer);
         const makeDiv = document.createElement('div');
@@ -27,7 +34,7 @@ const displayplayer = players => {
                     <p class="card-text">${singlePlayer.strDescriptionEN.slice(0, 120)}..</p>
                 </div>
                 <div class="card-footer d-flex justify-content-around">
-                    <a href="#" class="btn btn-danger me-2">Delete</a>
+                    <a onclick = "playerDelete(${singlePlayer.idPlayer}) href="#" class="btn btn-danger me-2">Delete</a>
                     <a onclick = "playerDetails(${singlePlayer.idPlayer})" href="#" class="btn btn-primary ms-2">Details</a>
                 </div>
             </div> `
@@ -35,6 +42,7 @@ const displayplayer = players => {
     }
 }
 
+//show Single player deatails info using there ID
 const playerDetails = playerId =>{
     //  console.log(playerId);
     fetch(`https://www.thesportsdb.com/api/v1/json/2/lookupplayer.php?id= ${playerId}`)
@@ -44,7 +52,9 @@ const playerDetails = playerId =>{
 
 const displayPlayerDetails = playerIdenty =>{
     //  console.log(playerIdenty);
-    console.log(playerIdenty.strPlayer);
+    const playerInfoDetails = document.getElementById('player-info');
+    // playerInfoDetails.textContent = '';
+
     if(playerIdenty.strGender == "Male"){
         // console.log("hello");
         document.getElementById('male-player').style.display = "block";
@@ -55,7 +65,6 @@ const displayPlayerDetails = playerIdenty =>{
         document.getElementById('male-player').style.display = "none";
     }
 
-    const playerInfoDetails = document.getElementById('player-info');
     playerInfoDetails.innerHTML =`
     <div class="card">
         <div class="card-body">
@@ -69,8 +78,6 @@ const displayPlayerDetails = playerIdenty =>{
     </div>
     `
 }
-
-
 
 // https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=Danny%20Welbeck
 // https://www.thesportsdb.com/api.php
